442c57b885c7516b0241ddc86fbad962d3a3a0ec
Diego Calvillo <diegocalvillordz@gmail.com>
2017-02-27 17:15:32 -0600

    Initial Commit

---
+++ MyTodoList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTodoList
+//
+//  Created by Ping Pong Innovation on 27/02/17.
+//  Copyright © 2017 Ping Pong Innovation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
